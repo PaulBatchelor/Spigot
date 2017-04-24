04859f375e65d8a030cd8d32aedb394002c2223b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-04-23 20:42:02 -0700

    load argument for headless mode

--- spigot.c
+++ spigot.c
@@ -6,6 +6,7 @@
 
 typedef struct {
     spigot_graphics *gfx;
+    int load;
 } spigot_stuff;
 
 static int sporth_spigot(plumber_data *pd, sporth_stack *stack, void **ud)
@@ -32,26 +33,33 @@ static int sporth_spigot(plumber_data *pd, sporth_stack *stack, void **ud)
             sporth_stack_pop_string(stack);
             sporth_stack_pop_float(stack);
             sporth_stack_pop_float(stack);
+            sporth_stack_pop_float(stack);
             sporth_stack_push_float(stack, 0.0); 
             break;
         case PLUMBER_INIT:
             stuff = *ud;
             filename = sporth_stack_pop_string(stack);
+            stuff->load = sporth_stack_pop_float(stack);
             sporth_stack_pop_float(stack);
             sporth_stack_pop_float(stack);
             spigot_init(filename);
-            spigot_start(stuff->gfx);
+            if(stuff->load != 0) {
+                spigot_start(stuff->gfx);
+            }
             sporth_stack_push_float(stack, 0.0);
             break;
 
         case PLUMBER_COMPUTE:
             stuff = *ud;
+            sporth_stack_pop_float(stack);
             in = sporth_stack_pop_float(stack);
             val = sporth_stack_pop_float(stack);
             spigot_constant(val);
             tick = 0;
             if(in != 0) {
+                if(stuff->load != 0) {
                 spigot_gfx_step(stuff->gfx);
+                }
                 tick = spigot_step();
             }
 
@@ -65,7 +73,9 @@ static int sporth_spigot(plumber_data *pd, sporth_stack *stack, void **ud)
 
         case PLUMBER_DESTROY:
             stuff = *ud;
-            spigot_stop(stuff->gfx);
+            if(stuff->load != 0) {
+                spigot_stop(stuff->gfx);
+            }
             spigot_gfx_free(stuff->gfx);
             free(stuff);
             break;
